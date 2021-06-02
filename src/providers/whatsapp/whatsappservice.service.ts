405c022ea78d99d5e8cdc0f712e996ee52ca5cd8
henrickJona <jhenrick41@gmail.com>
2021-06-02 08:36:16 -0300

    feat: add methods

--- src/providers/whatsapp/whatsappservice.service.ts
+++ src/providers/whatsapp/whatsappservice.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { create, Whatsapp } from 'venom-bot';
+import { create, Whatsapp, Contact, Id } from 'venom-bot';
 @Injectable()
 export class WhatsappService {
   private client: Whatsapp;
@@ -8,8 +8,41 @@ export class WhatsappService {
       console.log('Status Session: ', statusSession);
       console.log('Session name: ', session);
     })
-      .then((client) => {
+      .then(async (client) => {
         this.client = client;
+        //console.log(await this.client.getAllChats());
+        // await this.client
+        //   .sendImage(
+        //     '559691215506@c.us',
+        //     '/home/jonathan/Downloads/teste.jpg',
+        //     'teste',
+        //     'tetste',
+        //   )
+        //   .then((response) => {
+        //     console.log(response);
+        //   })
+        //   .catch((error) => {
+        //     console.log(error);
+        //   });
+
+        // console.log('ffff');
+        // await this.client
+        //   .sendVoice('559691215506@c.us', '/home/jonathan/Downloads/teste.mp3')
+        //   .then((response) => {
+        //     console.log(response);
+        //   })
+        //   .catch((error) => {
+        //     console.log(error);
+        //   });
+        // console.log(
+        //   await this.client.getBatteryLevel(),
+        //   await this.client.onStreamChange((state) => {
+        //     console.log(state);
+        //   }),
+        // );
+        process.on('SIGINT', function () {
+          client.close();
+        });
       })
       .catch((error) => {
         console.log(error);
@@ -17,6 +50,35 @@ export class WhatsappService {
   }
   async closeSession() {
     await this.client.close();
+  }
+
+  async getContactList(): Promise<Contact[]> {
+    return await this.client.getAllContacts();
+  }
+  async addNewContact(ContactId: Id) {
+    await this.client.getContact(ContactId.id);
+  }
+  async sendVoiceMessage(to: string, filePath: string) {
+    await this.client.sendVoice(to, filePath);
+  }
+  async sendStickerMessage(to: string, filePath: string) {
+    await this.client.sendImageAsSticker(to, filePath);
+  }
+  async sendImageMessage(
+    to: string,
+    filePath: string,
+    filename?: string,
+    caption?: string,
+  ) {
+    await this.client.sendImage(to, filePath, filename, caption);
+  }
+  async sendVideoMessage(
+    to: string,
+    path: string,
+    filename: string,
+    caption: string,
+  ) {
+    await this.client.sendVideoAsGif(to, path, filename, caption);
   }
 
   async listeningOnMessage() {
